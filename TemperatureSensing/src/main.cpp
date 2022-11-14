1f254896e6bda49e754cf7d291e65b7db85587a5
HedinGarcia <hedin.garcia@upr.edu>
2022-11-13 21:41:26 -0400

    Add template of temperature sensing project

---
+++ TemperatureSensing/src/main.cpp
@@ -0,0 +1,29 @@
+#include <WiFi.h>
+#include <stdio.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+// #include PubSubClient.h
+#include <driver/adc.h>
+
+void connectMQTT() {}
+
+void initializeWIFIandMQTT() {}
+
+void commission() {}
+
+int getTemperature()
+{
+  return 0;
+}
+
+void calculateAvegTemp() {}
+
+void callibration() {}
+
+void callback(char *topic, byte *payload, unsigned int length) {}
+
+void measureAndPublish(void *pvParameter) {}
+
+void setup() {}
+
+void loop() {}
