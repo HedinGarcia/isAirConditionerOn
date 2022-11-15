e6f8cbf9f5ce32660affeb4a3dbebde99f5e0dbb
HedinGarcia <hedin.garcia@upr.edu>
2022-11-14 23:51:04 -0400

    Implement function that connects the ESP32 to a WiFi network

--- TemperatureSensing/src/main.cpp
+++ TemperatureSensing/src/main.cpp
@@ -2,28 +2,46 @@
 #include <stdio.h>
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
-// #include PubSubClient.h
+#include <PubSubClient.h>
 #include <driver/adc.h>
 
-void connectMQTT() {}
+const char *wifi_network_name = "RUMNET";
+const char *wifi_network_password = "Colegio2019";
 
-void initializeWIFIandMQTT() {}
+void connect_mqtt() {}
+
+void initialize_wifi() {
+  delay(1000);
+  Serial.printf("Creating a connection to the \"%s\" Wi-Fi network:\n", wifi_network_name);
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(wifi_network_name, wifi_network_password);
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.print(".");
+    delay(1000);
+  }
+  Serial.printf("\nConnection made to the \"%s\" Wi-Fi network.\n", wifi_network_name);
+}
+
+void initialize_mqtt() {
+}
 
 void commission() {}
 
-int getTemperature()
-{
+int get_aveg_temp() {
   return 0;
 }
 
-void calculateAvegTemp() {}
+void calculate_aveg_temp() {}
 
 void callibration() {}
 
 void callback(char *topic, byte *payload, unsigned int length) {}
 
-void measureAndPublish(void *pvParameter) {}
+void measure_and_publish(void *pvParameter) {}
 
-void setup() {}
+void setup() {
+  Serial.begin(115200);
+  initialize_wifi();
+}
 
 void loop() {}
